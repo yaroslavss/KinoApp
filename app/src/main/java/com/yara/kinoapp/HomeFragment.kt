fe3e38ed35e95deeab36f0af0be45244980f3e67
yaroslavss <yaroslav.syuzev@gmail.com>
2023-04-19 16:00:25 +0400

    optimized import

--- app/src/main/java/com/yara/kinoapp/HomeFragment.kt
+++ app/src/main/java/com/yara/kinoapp/HomeFragment.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yara.kinoapp.databinding.FragmentHomeBinding
-import java.util.*
+import java.util.Locale
 
 class HomeFragment : Fragment() {
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
