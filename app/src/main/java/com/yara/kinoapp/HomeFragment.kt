b901863ae0ea069d747de8f2936a5c5201be5e61
yaroslavss <yaroslav.syuzev@gmail.com>
2023-04-24 14:30:18 +0400

    added home screen animation with transition framework

--- app/src/main/java/com/yara/kinoapp/HomeFragment.kt
+++ app/src/main/java/com/yara/kinoapp/HomeFragment.kt
@@ -1,6 +1,11 @@
 package com.yara.kinoapp
 
 import android.os.Bundle
+import android.transition.Scene
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.transition.TransitionSet
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +13,13 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yara.kinoapp.databinding.FragmentHomeBinding
+import com.yara.kinoapp.databinding.MergeHomeScreenContentBinding
 import java.util.Locale
 
 class HomeFragment : Fragment() {
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding2: MergeHomeScreenContentBinding
 
     private val filmsDataBase = listOf(
         Film(
@@ -52,23 +59,44 @@ class HomeFragment : Fragment() {
         )
     )
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding2 = MergeHomeScreenContentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.searchView.setOnClickListener {
-            binding.searchView.isIconified = false
+        val scene = Scene.getSceneForLayout(binding.homeFragmentRoot, R.layout.merge_home_screen_content, requireContext())
+
+        // search view animation
+        val searchSlide = Slide(Gravity.TOP).addTarget(R.id.search_view)
+        // RV animation
+        val recyclerSlide = Slide(Gravity.BOTTOM).addTarget(R.id.main_recycler)
+
+        val customTransition = TransitionSet().apply {
+            duration = 2000
+            addTransition(recyclerSlide)
+            addTransition(searchSlide)
         }
 
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        TransitionManager.go(scene, customTransition)
+
+        binding2.searchView.setOnClickListener {
+            binding2.searchView.isIconified = false
+        }
+
+        binding2.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return true
             }
@@ -92,7 +120,7 @@ class HomeFragment : Fragment() {
 
     private fun initRecycler() {
         // get RV
-        binding.mainRecycler.apply {
+        binding2.mainRecycler.apply {
             filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
                 override fun click(film: Film) {
                     (requireActivity() as MainActivity).launchDetailsFragment(film)
