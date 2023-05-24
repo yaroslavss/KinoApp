abfa5fad3d123e78008a1d0d469d771409057424
yaroslavss <yaroslav.syuzev@gmail.com>
2023-05-24 10:57:20 +0400

    m33: checked comments

--- app/src/main/java/com/yara/kinoapp/utils/AnimationHelper.kt
+++ app/src/main/java/com/yara/kinoapp/utils/AnimationHelper.kt
@@ -9,23 +9,22 @@ import kotlin.math.hypot
 import kotlin.math.roundToInt
 
 object AnimationHelper {
-    //Это переменная для того, что бы круг проявления расходился именно от иконки меню навигации
+    //Это переменная для того, чтобы круг проявления расходился именно от иконки меню навигации
     private const val menuItems = 4
     //В метод у нас приходит 3 параметра:
-    //1 - наше rootView, которое одновременно является и контейнером
-    //и объектом анимации
-    //2 - активити, для того чтобы вернуть выполнение нового треда в UI поток
-    //3 - позиция в меню навигации, что бы круг проявления расходился именно от иконки меню навигации
+    //1 - наше rootView, которое одновременно является и контейнером, и объектом анимации
+    //2 - активити, для того, чтобы вернуть выполнение нового треда в UI поток
+    //3 - позиция в меню навигации, чтобы круг проявления расходился именно от иконки меню навигации
     fun performFragmentCircularRevealAnimation(rootView: View, activity: Activity, position: Int) {
         //Создаем новый тред
         Executors.newSingleThreadExecutor().execute {
             //В бесконечном цикле проверям, когда наше анимируемое view будет "прикреплено" к экрану
             while (true) {
-                //Когда оно будет прикреплено выполним код
+                //Когда оно будет прикреплено, выполним код
                 if (rootView.isAttachedToWindow) {
                     //Возвращаемся в главный тред, чтобы выполнить анимацию
                     activity.runOnUiThread {
-                        //Cупер сложная математика вычесления старта анимации
+                        //Сложная математика вычисления старта анимации
                         val itemCenter = rootView.width / (menuItems * 2)
                         val step = (itemCenter * 2) * (position - 1) + itemCenter
 
@@ -38,12 +37,12 @@ object AnimationHelper {
                         ViewAnimationUtils.createCircularReveal(rootView, x, y, startRadius.toFloat(), endRadius.toFloat()).apply {
                             //Устанавливаем время анимации
                             duration = 500
-                            //Интерполятор для более естесственной анимации
+                            //Интерполятор для более естественной анимации
                             interpolator = AccelerateDecelerateInterpolator()
                             //Запускаем
                             start()
                         }
-                        //Выставляяем видимость нашего елемента
+                        //Выставляем видимость нашего элемента
                         rootView.visibility = View.VISIBLE
                     }
                     return@execute
