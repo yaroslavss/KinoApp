2a4e19f9c154dca4e0f58c570d0126e4a2e5d341
yaroslavss <yaroslav.syuzev@gmail.com>
2023-07-28 11:02:04 +0400

    m42: added saving of film's poster into gallery with coroutines

--- app/src/main/java/com/yara/kinoapp/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/yara/kinoapp/view/fragments/DetailsFragment.kt
@@ -1,19 +1,32 @@
 package com.yara.kinoapp.view.fragments
 
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import com.yara.kinoapp.R
 import com.yara.kinoapp.databinding.FragmentDetailsBinding
 import com.yara.kinoapp.domain.Film
+import com.yara.kinoapp.viewmodel.DetailsFragmentViewModel
+import kotlinx.coroutines.*
 
 class DetailsFragment : Fragment() {
     private lateinit var binding: FragmentDetailsBinding
     private lateinit var film: Film
+    private val viewModel by lazy {
+        ViewModelProvider.NewInstanceFactory().create(DetailsFragmentViewModel::class.java)
+    }
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +41,7 @@ class DetailsFragment : Fragment() {
 
         setFilmsDetails()
 
+        // proceed clicking on 'Add to favorites' button
         binding.detailsFabFavorites.setOnClickListener {
             if (!film.isInFavorites) {
                 binding.detailsFabFavorites.setImageResource(R.drawable.ic_baseline_favorite_24)
@@ -38,6 +52,7 @@ class DetailsFragment : Fragment() {
             }
         }
 
+        // proceed clicking on 'Share' button
         binding.detailsFabShare.setOnClickListener {
             // create intent
             val intent = Intent()
@@ -54,6 +69,15 @@ class DetailsFragment : Fragment() {
             startActivity(Intent.createChooser(intent, "Share To:"))
         }
 
+        // proceed clicking on 'Save to gallery' button
+        binding.detailsFabDownloadWp.setOnClickListener {
+            performAsyncLoadOfPoster()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
     }
 
     private fun setFilmsDetails() {
@@ -72,5 +96,87 @@ class DetailsFragment : Fragment() {
             if (film.isInFavorites) R.drawable.ic_baseline_favorite_24
             else R.drawable.ic_baseline_favorite_border_24
         )
+    }
+
+    private fun performAsyncLoadOfPoster() {
+        // create parent scope in Main Thread to proceed with UI
+        MainScope().launch {
+            // set progress bar on
+            binding.progressBar.isVisible = true
+            // create new job to get bitmap
+            val job = scope.async {
+                viewModel.loadWallpaper(film.poster)
+            }
+            // save to gallery after file was downloaded
+            saveToGallery(job.await())
+            // show snack bar with button
+            Snackbar.make(
+                binding.root,
+                R.string.downloaded_to_gallery,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.open) {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.type = "image/*"
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                .show()
+
+            // set progress bar off
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    private fun saveToGallery(bitmap: Bitmap) {
+        // check Android version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // create object to pass
+            val contentValues = ContentValues().apply {
+                // fill in file attributes
+                put(MediaStore.Images.Media.TITLE, film.title.handleSingleQuote())
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    film.title.handleSingleQuote()
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(
+                    MediaStore.Images.Media.DATE_ADDED,
+                    System.currentTimeMillis() / 1000
+                )
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, RELATIVE_PATH)
+            }
+            // get ContentResolver to deal with persistent storage
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            // open stream to write on disk
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            // put poster with compression
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            // close stream
+            outputStream?.close()
+        } else {
+            // for older Android versions
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                film.title.handleSingleQuote(),
+                film.description.handleSingleQuote()
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
+    }
+
+    companion object {
+        const val RELATIVE_PATH = "Pictures/KinoApp"
     }
 }
