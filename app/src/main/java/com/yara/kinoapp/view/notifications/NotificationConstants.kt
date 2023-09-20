2c85d02fcc2bc1a579739e9fb9f46e54d13e2d8e
yaroslavss <yaroslav.syuzev@gmail.com>
2023-09-20 14:55:49 +0400

    now setting pending notification with AlarmManager

--- app/src/main/java/com/yara/kinoapp/view/notifications/NotificationConstants.kt
+++ app/src/main/java/com/yara/kinoapp/view/notifications/NotificationConstants.kt
@@ -2,4 +2,6 @@ package com.yara.kinoapp.view.notifications
 
 object NotificationConstants {
     const val CHANNEL_ID = "KinoAppChannel"
+    const val FILM_BUNDLE_KEY = "FILM_BUNDLE"
+    const val FILM_KEY = "FILM_KEY"
 }
