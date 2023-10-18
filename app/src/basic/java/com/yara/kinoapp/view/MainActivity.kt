abf6f78121346ccde79673f133df454596490b3d
yaroslavss <yaroslav.syuzev@gmail.com>
2023-10-18 13:48:44 +0400

    m52: added flavors - Pro and Basic versions

---
+++ app/src/basic/java/com/yara/kinoapp/view/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.yara.kinoapp.view
+
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.yara.kinoapp.R
+import com.yara.kinoapp.databinding.ActivityMainBinding
+import com.yara.kinoapp.domain.Film
+import com.yara.kinoapp.receivers.ConnectionChecker
+import com.yara.kinoapp.view.fragments.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
+    private var backPressed = 0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+
+        // run fragment on start
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_placeholder, HomeFragment())
+            .addToBackStack(null)
+            .commit()
+
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount == 1) {
+            if (backPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+                super.onBackPressed()
+                finish()
+            } else {
+                Toast.makeText(this, "Double tap to exit", Toast.LENGTH_SHORT).show()
+            }
+
+            backPressed = System.currentTimeMillis()
+        } else
+            super.onBackPressed()
+    }
+
+    fun launchDetailsFragment(film: Film) {
+        val bundle = Bundle()
+        bundle.putParcelable("film", film)
+        val fragment = DetailsFragment()
+        fragment.arguments = bundle
+
+        // run fragment
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun initNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    val tag = "home"
+                    val fragment = checkFragmentExistence(tag)
+                    // if fragment not found (null) create new one
+                    changeFragment( fragment?: HomeFragment(), tag)
+                    true
+                }
+                R.id.favorites -> {
+                    Toast.makeText(this, R.string.toast_pro_version, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    val tag = "watch_later"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: WatchLaterFragment(), tag)
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, R.string.toast_pro_version, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.settings -> {
+                    val tag = "settings"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: SettingsFragment(), tag)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // find Fragment by tag
+    private fun checkFragmentExistence(tag: String): Fragment? = supportFragmentManager.findFragmentByTag(tag)
+
+    private fun changeFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment, tag)
+            .addToBackStack(null)
+            .commit()
+    }
+
+
+    companion object {
+        const val TIME_INTERVAL = 2000
+    }
+}
